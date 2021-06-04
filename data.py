6702d4e37cc39f28b959de4396ff2b6efbd7b95a
kinggodhj <heeeee928@gamil.com>
2021-06-04 17:44:15 +0900

    learn baseline

--- data.py
+++ data.py
@@ -1,6 +1,7 @@
 import torch
+from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import Dataset
-#import pdb
+import pdb
 
 class SequenceDataset(Dataset):
     #def __init__(self, source_file, target_file, batch_size, max_len, voca1, voca2):
@@ -23,23 +24,38 @@ class SequenceDataset(Dataset):
         return len(self.y)
 
     def __getitem__(self, idx):
-        x= [self.bos] + [self.voca_x[word] for word in self.x[idx]] + [self.eos]
-        y = [self.bos] + [self.voca_y[word] for word in self.y[idx]] + [self.eos]
+        x= [self.voca_x[word] for word in self.x[idx]]
+        y = [self.voca_y[word] for word in self.y[idx]] 
+        #x += [self.pad] * (self.max_len - len(x))
+        #y += [self.pad] * (self.max_len - len(y))
+	
+        #x = [self.bos] + x + [self.eos]
+        #y = [self.bos] + y + [self.bos]
 
-        x += [self.pad] * (self.max_len - len(x))
-        y += [self.pad] * (self.max_len - len(y))
+        return torch.tensor(x), torch.tensor(y)
 
-        return x, y
-
+    '''
     def get_batch(self, idx, batch):
         batch_x = []
         batch_y = []
         for i in range(idx * batch, (idx+1) * batch):
             x, y = self.__getitem__(i)
-            batch_x.append(torch.Tensor(x))
-            batch_y.append(torch.Tensor(y))
+            batch_x.append(torch.Tensor(x).long())
+            batch_y.append(torch.Tensor(y).long())
+        batch_x = pad_sequence(batch_x, padding_value=self.pad)
+        batch_y = pad_sequence(batch_x, padding_value=self.pad)
         
         return torch.stack(batch_x), torch.stack(batch_y)
+    '''
 
+    def get_batch(self, data_batch):
+        batch_x, batch_y = [], []
+        for (x_item, y_item) in data_batch:
+            batch_x.append(torch.cat([torch.tensor([self.bos]), x_item, torch.tensor([self.eos])], dim=0))
+            batch_y.append(torch.cat([torch.tensor([self.bos]), y_item, torch.tensor([self.eos])], dim=0))
+
+        batch_x = pad_sequence(batch_x, padding_value=self.pad)
+        batch_y = pad_sequence(batch_y, padding_value=self.pad)
+        return batch_x, batch_y
 #data = SequenceDataset('./train_source.txt', 'train_target.txt')
 #print(data.__len__())
--- main.py
+++ main.py
@@ -1,17 +1,123 @@
-from prepare import setup
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+import time
 import pdb
 
-source_file = "./train_source.txt"
-target_file = "./train_target.txt"
-batch_size = 64
-max_len = 100
+from model import Seq2SeqTransformer, create_mask
+from prepare import setup
 
-train_data, val_data, voca_x, voca_y = setup(source_file, target_file, max_len)
-EOS = voca_x['<eos>']
-BOS = voca_x['<bos>']
-PAD = voca_x['<pad>']
+DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-for i in range(len(train_data)//batch_size):
-    train_x, train_y = train_data.get_batch(i, batch_size)
-pdb.set_trace()
-print(train_x)
+parser = argparse.ArgumentParser()
+parser.add_argument('--batch_size', type=int, default=128)
+parser.add_argument('--max_len', type=int, default=50)
+parser.add_argument('--emb_size', type=int, default=512)
+parser.add_argument('--nhead', type=int, default=8)
+parser.add_argument('--ffd_dim', type=int, default=512)
+parser.add_argument('--num_encoder_layers', type=int, default=3)
+parser.add_argument('--num_decoder_layers', type=int, default=3)
+parser.add_argument('--epochs', type=int, default=20)
+
+args = parser.parse_args()
+
+MAX_LEN = args.max_len
+EMB_SIZE = args.emb_size
+NHEAD = args.nhead
+FFN_HID_DIM = args.ffd_dim
+BATCH_SIZE = args.batch_size
+NUM_ENCODER_LAYERS = args.num_encoder_layers
+NUM_DECODER_LAYERS = args.num_decoder_layers
+NUM_EPOCHS = args.epochs
+
+writer = SummaryWriter()
+
+def train(model, train_iter, optimizer):
+    model.train()
+    losses = 0
+    #for i in range(len(train_data) //BATCH_SIZE):
+    #    src, tgt = train_data.get_batch(i, BATCH_SIZE)
+    for idx, (src, tgt) in enumerate(train_iter):
+        src = src.to(DEVICE)
+        tgt = tgt.to(DEVICE)
+
+        tgt_input = tgt[:-1, :]
+
+        src_mask, tgt_mask, src_padding_mask, tgt_padding_mask = create_mask(src, tgt_input, PAD)
+
+        logits = model(src, tgt_input, src_mask, tgt_mask, src_padding_mask, tgt_padding_mask, src_padding_mask)
+
+        optimizer.zero_grad()
+
+        tgt_out = tgt[1:,:]
+        #pdb.set_trace()
+        loss = loss_fn(logits.reshape(-1, logits.shape[-1]), tgt_out.reshape(-1))
+        loss.backward()
+
+        optimizer.step()
+        losses += loss.item()
+    return model, (losses / len(train_iter))
+
+
+def evaluate(model, val_iter):
+    model.eval()
+    losses = 0
+    #for i in range(len(val_data) // BATCH_SIZE):
+    #    src, tgt = val_data.get_batch(i, BATCH_SIZE)
+    for idx, (src, tgt) in enumerate(val_iter):
+        src = src.to(DEVICE)
+        tgt = tgt.to(DEVICE)
+
+        tgt_input = tgt[:-1, :]
+
+        src_mask, tgt_mask, src_padding_mask, tgt_padding_mask = create_mask(src, tgt_input, PAD)
+
+        logits = model(src, tgt_input, src_mask, tgt_mask, src_padding_mask, tgt_padding_mask, src_padding_mask)
+        tgt_out = tgt[1:,:]
+        loss = loss_fn(logits.reshape(-1, logits.shape[-1]), tgt_out.reshape(-1))
+        losses += loss.item()
+    return losses / len(val_iter)
+
+
+if __name__ == "__main__":
+    source_file = "./train_source.txt"
+    target_file = "./train_target.txt"
+
+    train_data, val_data, voca_x, voca_y = setup(source_file, target_file, MAX_LEN)
+    
+    train_iter = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True, collate_fn=train_data.get_batch)
+    val_iter = DataLoader(val_data, batch_size=BATCH_SIZE, shuffle=True, collate_fn=val_data.get_batch)
+    
+    SRC_VOCAB_SIZE = len(voca_x)
+    TGT_VOCAB_SIZE = len(voca_y)
+
+    EOS = voca_x['<eos>']
+    BOS = voca_x['<bos>']
+    PAD = voca_x['<pad>']
+
+    transformer = Seq2SeqTransformer(NUM_ENCODER_LAYERS, NUM_DECODER_LAYERS, NHEAD, EMB_SIZE, SRC_VOCAB_SIZE, TGT_VOCAB_SIZE, FFN_HID_DIM)
+
+    '''
+    for p in transformer.parameters():
+        if p.dim() > 1:
+            nn.init.xavier_uniform_(p)    
+    '''
+
+    transformer = transformer.to(DEVICE)
+
+    loss_fn = torch.nn.CrossEntropyLoss(ignore_index=PAD)
+    optimizer = torch.optim.Adam(transformer.parameters(), lr=0.0001, betas=(0.9, 0.98), eps=1e-9)
+    #pdb.set_trace()
+
+    for epoch in range(1, NUM_EPOCHS+1):
+        start_time = time.time()
+        transformer, train_loss = train(transformer, train_iter, optimizer)
+        end_time = time.time()
+        val_loss = evaluate(transformer, val_iter)
+        print((f"Epoch: {epoch}, Train loss: {train_loss:.3f}, Val loss: {val_loss:.3f}, " f"Epoch time = {(end_time - start_time):.3f}s"))
+        writer.add_scalar('Loss/train', train_loss, epoch)
+        writer.add_scalar('Loss/Val', val_loss, epoch)
+    torch.save(transformer.state_dict(), './model/model%s.pkt'%(NUM_EPOCHS))
